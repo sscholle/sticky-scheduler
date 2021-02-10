4dc23e0419570633dd72443af50db3b6704b0f38
Sebastian Scholle <sscholle@gmail.com>
2021-02-10 09:42:10 +0200

    Commit StickySurface and Sticky Component files - added basic comments in files describing interesting properties and  functions

---
+++ src/components/Sticky.js
@@ -0,0 +1,6 @@
+/**
+ * Sticky
+ * properties that affect style: position[x,y], Assignee.color
+ * properties that affect classes:  priority, complete
+ * other props: Assignee.name, taskText
+ */
---
+++ src/components/StickySurface.js
@@ -0,0 +1,8 @@
+/**
+ * StickySurface
+ * this will listen for click events on the main background surface
+ * each click generates a sticky data entry in state
+ * each sticky can be edited live (or when user focus is captured)
+ * also display a list of Team Member
+ * add action button to add a new team member via modal/dialog
+ */
